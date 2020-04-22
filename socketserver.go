5902dfc704951b006fc8212a8b342120cb024040
ShushingWong <122181582@qq.com>
2020-04-22 11:06:11 +0800

    first commit

---
+++ socketserver.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func Listening() {
+	tcpListen, err := net.Listen("tcp", ":8086")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := tcpListen.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go connHandle(conn)
+	}
+}
+func  connHandle(conn net.Conn) {
+	defer conn.Close()
+	readBuff := make([]byte, 1024)
+	for {
+		n, err := conn.Read(readBuff)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		conn.Write([]byte("hello too\n"))
+		fmt.Println(readBuff[:n])
+
+	}
+}
